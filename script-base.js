b87a09be70fc6fcf5e05106ce41c5cde661a8eb2
Lyu12345 <121919364+Lyu12345@users.noreply.github.com>
2023-02-26 15:00:54 +0900

    Update script-base.js

--- script-base.js
+++ script-base.js
@@ -123,7 +123,10 @@ class App {
         loader.load('./assets/hand01.gltf', (gltf) => {
             hand01 = gltf.scene;
             hand01.position.set(hand01Pos.x, hand01Pos.y, hand01Pos.z);
-            arm4.add(hand01);
+                        // arm4.add(hand01);
+            if (hand01) {
+                arm4.add(hand01);
+            }
             this._gui.add(hand01.rotation, 'y', -0.5 * Math.PI, 0.5 * Math.PI, 0.01)
                 .name('hand Z')
                 .onChange(() => {
